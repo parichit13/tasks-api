78d2d643b81693045bf8f523da690e61f456e509
Parichit <parichit.kumark@gmail.com>
2018-01-19 00:43:24 +0530

    Fixes

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask.ext.heroku import Heroku
+from flask_cors import CORS
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:start@localhost/temp2'
+CORS(app)
 heroku = Heroku(app)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
